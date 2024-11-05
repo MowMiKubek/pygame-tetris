9ebb635ffede648bf725f4f31c13f3f527b3a390
Jakub Maciej Tkaczyk <mowmikubek@gmail.com>
2024-11-05 19:51:18 +0100

    Define set of tetrominoes. Add draw_tetromino function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 pygame.init()
@@ -23,30 +25,84 @@ WHITE = (255, 255, 255)
 #     [0, 0, 0, 0],
 # ]
 
+"""
+    ####    - I
+    
+    ###
+     #      - T
+     
+    ###
+    #       - L
+    
+    ###
+      #     - J 
+    
+    ##      - O
+    ##
+"""
+
+tetrominoes = [
+    [[1, 1, 1, 1]],  # I
+    [[1, 1, 1], [0, 1, 0]],  # T
+    [[1, 1, 1], [1, 0, 0]],  # L
+    [[1, 1, 1], [0, 0, 1]],  # J
+    [[1, 1], [1, 1]],  # O
+]
+
+COLORS = [
+    (0, 255, 255),  # I
+    (128, 0, 128),  # T
+    (255, 165, 0),  # L
+    (0, 0, 255),    # J
+    (255, 255, 0),  # O
+]
+
+
+class Tetromino:
+    def __init__(self):
+        self.shape = random.choice(tetrominoes)
+        self.color = random.choice(COLORS)
+        self.x = GRID_WIDTH // 2 - len(self.shape[0]) // 2
+        self.y = 0
+        self.rotation = 0
+
+
 def draw_grid(surface, grid):
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
             if grid[y][x] != 0:
                 pygame.draw.rect(surface, grid[y][x], (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(surface, WHITE, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 1)
+            # pygame.draw.rect(surface, WHITE, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 1)
+
+
+
+def draw_tetromino(surface, tetromino):
+    for y, row in enumerate(tetromino.shape):
+        for x, cell in enumerate(row):
+            if cell != 0:
+                pygame.draw.rect(surface, tetromino.color,
+                                 ((tetromino.x + x) * BLOCK_SIZE, (tetromino.y + y) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
 
 
 grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
-grid[8][7] = (125, 65, 9)
-grid[7][7] = (125, 65, 9)
-grid[6][7] = (125, 65, 9)
-grid[6][6] = (125, 65, 9)
 
 
 running = True
+current_tetromino = Tetromino()
+
+
 while running:
     screen.fill(BLACK)
-
     clock.tick()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                current_tetromino.y += 1
 
     draw_grid(screen, grid)
+    draw_tetromino(screen, current_tetromino)
     pygame.display.flip()
