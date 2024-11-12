3ef784a491c8a397999ab174971c9daa79c0077c
Jakub Maciej Tkaczyk <mowmikubek@gmail.com>
2024-11-12 19:48:55 +0100

    Add falling and hit ground feature

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ WHITE = (255, 255, 255)
 #     [0, (125,65,9), 0, 0],
 #     [0, 0, 0, 0],
 #     [0, 0, 0, 0],
-#     [0, 0, 0, 0],
-#     [0, 0, 0, 0],
-#     [0, 0, 0, 0],
+#     [0, 1, 1, 0],
+#     [0, 1, 0, 0],
+#     [0, 1, 0, 0],
 # ]
 
 """
@@ -92,9 +92,24 @@ grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
 running = True
 current_tetromino = Tetromino()
 
+fall_speed = 500
+fall_time = 0
+
 while running:
     screen.fill(BLACK)
+    fall_time += clock.get_rawtime()
     clock.tick()
+
+    if fall_time >= fall_speed:
+        fall_time = 0
+        if current_tetromino.y + len(current_tetromino.shape) + 1 > GRID_HEIGHT:
+            for y, row in enumerate(current_tetromino.shape):
+                for x, cell in enumerate(row):
+                    if cell != 0:
+                        grid[current_tetromino.y + y][current_tetromino.x + x] = current_tetromino.color
+            current_tetromino = Tetromino()
+        else:
+            current_tetromino.y += 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
