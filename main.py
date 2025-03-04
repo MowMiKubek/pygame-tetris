6667bf3782389ad63ed9ab71ea24f1125a3ae85a
Jakub Maciej Tkaczyk <mowmikubek@gmail.com>
2025-03-04 19:35:45 +0100

    Refactor: Export Tetromino to other class

--- main.py
+++ main.py
@@ -1,114 +1,39 @@
-import random
 from copy import deepcopy
 import pygame
+import util.constants as constants
+import util.Tetromino as Tetromino
 
 pygame.init()
 clock = pygame.time.Clock()
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
-GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT = 300, 600
-GRID_WIDTH, GRID_HEIGHT = 10, 20
-BLOCK_SIZE = 30
+window = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+game_screen = pygame.Surface((constants.GAME_SCREEN_WIDTH, constants.GAME_SCREEN_HEIGHT))
 
-window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-game_screen = pygame.Surface((GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT))
-
-
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 
 score_font = pygame.font.SysFont('Arial', 36)
-
-# grid = [
-#     [0, 0, 0, 0],
-#     [0, 0, 0, 0],
-#     [0, (125,65,9), 0, 0],
-#     [0, 0, 0, 0],
-#     [0, 0, 0, 0],
-#     [0, 1, 1, 0],
-#     [0, 1, 0, 0],
-#     [0, 1, 0, 0],
-# ]
-
-"""
-    ####    - I
-    
-    ###
-     #      - T
-     
-    ###
-    #       - L
-    
-    ###
-      #     - J 
-    
-    ##      - O
-    ##
-"""
-
-tetrominoes = [
-    [[1, 1, 1, 1]],  # I
-    [[1, 1, 1], [0, 1, 0]],  # T
-    [[1, 1, 1], [1, 0, 0]],  # L
-    [[1, 1, 1], [0, 0, 1]],  # J
-    [[1, 1], [1, 1]],  # O
-    [[0, 1, 1], [1, 1, 0]],
-    # [[0, 1, 0],[1, 1, 1],[0, 1, 0]]
-]
-
-COLORS = [
-    (0, 255, 255),  # I
-    (128, 0, 128),  # T
-    (255, 165, 0),  # L
-    (0, 0, 255),  # J
-    (255, 255, 0),  # O
-]
-
-
-class Tetromino:
-    def __init__(self, forbidden_shape=None):
-        self.shape = random.choice(list(filter(lambda elem: elem != forbidden_shape, tetrominoes)))
-        self.color = random.choice(COLORS)
-        self.x = GRID_WIDTH // 2 - len(self.shape[0]) // 2
-        self.y = 0
-        self.rotation = 0
-
-    def rotate(self):
-        self.shape = [list(row) for row in zip(*self.shape[::-1])]
-
 
 def valid_move(shape, grid, offset):
     off_x, off_y = offset
     for y, row in enumerate(shape):
         for x, cell in enumerate(row):
             if cell != 0:
-                if x + off_x < 0 or x + off_x >= GRID_WIDTH or y + off_y >= GRID_HEIGHT:
+                if x + off_x < 0 or x + off_x >= constants.GRID_WIDTH or y + off_y >= constants.GRID_HEIGHT:
                     return False
                 if grid[y + off_y][x + off_x] != 0:
                     return False
     return True
 
 def draw_grid(surface, grid):
-    for y in range(GRID_HEIGHT):
-        for x in range(GRID_WIDTH):
+    for y in range(constants.GRID_HEIGHT):
+        for x in range(constants.GRID_WIDTH):
             if grid[y][x] != 0:
-                pygame.draw.rect(surface, grid[y][x], (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
-            # pygame.draw.rect(surface, WHITE, (x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 1)
-
-
-def draw_tetromino(surface, tetromino):
-    for y, row in enumerate(tetromino.shape):
-        for x, cell in enumerate(row):
-            if cell != 0:
-                pygame.draw.rect(surface, tetromino.color,
-                                 ((tetromino.x + x) * BLOCK_SIZE, (tetromino.y + y) * BLOCK_SIZE, BLOCK_SIZE,
-                                  BLOCK_SIZE))
+                pygame.draw.rect(surface, grid[y][x], (x * constants.BLOCK_SIZE, y * constants.BLOCK_SIZE,
+                                                       constants.BLOCK_SIZE, constants.BLOCK_SIZE))
 
 def clear_lines(grid):
     new_grid = [row for row in grid if any(cell == 0 for cell in row)] # 111101 - keep it; 111111 - discard it
-    lines_removed = GRID_HEIGHT - len(new_grid)
-    new_grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(lines_removed)] + new_grid
+    lines_removed = constants.GRID_HEIGHT - len(new_grid)
+    new_grid = [[0 for _ in range(constants.GRID_WIDTH)] for _ in range(lines_removed)] + new_grid
     return new_grid, lines_removed
 
 def check_if_game_over(grid, tetromino):
@@ -131,19 +56,20 @@ def draw_next_tetromino(surface, tetromino, location):
         for x, cell in enumerate(row):
             if cell != 0:
                 pygame.draw.rect(surface, tetromino.color,
-                                 ((x_loc + x) * BLOCK_SIZE, (y_loc + y) * BLOCK_SIZE, BLOCK_SIZE,
-                                  BLOCK_SIZE))
+                                 ((x_loc + x) * constants.BLOCK_SIZE, (y_loc + y) * constants.BLOCK_SIZE,
+                                  constants.BLOCK_SIZE,
+                                  constants.BLOCK_SIZE))
 
 def split(text):
     separator_idx = text.find(':')
     return text[:separator_idx], text[separator_idx+1:]
 
 
-grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+grid = [[0 for _ in range(constants.GRID_WIDTH)] for _ in range(constants.GRID_HEIGHT)]
 
 running = True
-current_tetromino = Tetromino()
-next_tetromino = Tetromino(current_tetromino.shape)
+current_tetromino = Tetromino.Tetromino()
+next_tetromino = Tetromino.Tetromino(current_tetromino.shape)
 ghost_tetromino = deepcopy(current_tetromino)
 ghost_tetromino.color = (20, 20, 20)
 ghost_tetromino = place_ghost(grid, ghost_tetromino)
@@ -161,7 +87,7 @@ fall_time = 0
 game_score = 0
 
 while running:
-    game_screen.fill(BLACK)
+    game_screen.fill(constants.BLACK)
     fall_time += clock.get_rawtime()
     clock.tick()
 
@@ -194,7 +120,7 @@ while running:
             elif lines == 4:
                 game_score += 1200
             current_tetromino = next_tetromino
-            next_tetromino = Tetromino(current_tetromino.shape)
+            next_tetromino = Tetromino.Tetromino(current_tetromino.shape)
             if not check_if_game_over(grid, current_tetromino):
                 running = False
 
@@ -222,9 +148,8 @@ while running:
     ghost_tetromino = place_ghost(grid, ghost_tetromino)
 
     draw_grid(game_screen, grid)
-    draw_tetromino(game_screen, ghost_tetromino)
-    draw_tetromino(game_screen, current_tetromino)
-
+    Tetromino.Tetromino.draw_tetromino(game_screen, ghost_tetromino)
+    Tetromino.Tetromino.draw_tetromino(game_screen, current_tetromino)
 
     text_score = score_font.render(f"Score: {game_score}", True, (255, 255, 255))
     text_rect = text_score.get_rect(topleft=(0, 0))
@@ -233,8 +158,7 @@ while running:
 
     window.fill((20, 20, 20))
     draw_next_tetromino(window, next_tetromino, (20, 5))
-    window.blit(game_screen, ((SCREEN_WIDTH - GAME_SCREEN_WIDTH) // 2, 0))
-
+    window.blit(game_screen, ((constants.SCREEN_WIDTH - constants.GAME_SCREEN_WIDTH) // 2, 0))
 
     pygame.display.flip()
 
@@ -251,21 +175,21 @@ while SCORE_ENTER:
             pygame.quit()
 
     window.fill((20, 20, 20))
-    window.blit(game_screen, ((SCREEN_WIDTH - GAME_SCREEN_WIDTH) // 2, 0))
+    window.blit(game_screen, ((constants.SCREEN_WIDTH - constants.GAME_SCREEN_WIDTH) // 2, 0))
 
     font_game_over = pygame.font.SysFont("Comic Sans MS", 50)
     font_player = pygame.font.SysFont("Comic Sans MS", 30)
 
     text_game_over = font_game_over.render(f"Game Over", True, (255, 0, 0))
-    text_rect = text_game_over.get_rect(center=(GAME_SCREEN_WIDTH // 2, GAME_SCREEN_HEIGHT // 2))
+    text_rect = text_game_over.get_rect(center=(constants.GAME_SCREEN_WIDTH // 2, constants.GAME_SCREEN_HEIGHT // 2))
     window.blit(text_game_over, text_rect)
 
     text_player = font_player.render(f"Your name:", True, (255, 255, 255))
-    text_rect = text_game_over.get_rect(center=(GAME_SCREEN_WIDTH // 2, GAME_SCREEN_HEIGHT // 2 + 75))
+    text_rect = text_game_over.get_rect(center=(constants.GAME_SCREEN_WIDTH // 2, constants.GAME_SCREEN_HEIGHT // 2 + 75))
     window.blit(text_player, text_rect)
 
     text_player = font_player.render(f"{player_name}", True, (255, 255, 255))
-    text_rect = text_game_over.get_rect(center=(GAME_SCREEN_WIDTH // 2, GAME_SCREEN_HEIGHT // 2 + 150))
+    text_rect = text_game_over.get_rect(center=(constants.GAME_SCREEN_WIDTH // 2, constants.GAME_SCREEN_HEIGHT // 2 + 150))
     window.blit(text_player, text_rect)
 
     for event in pygame.event.get():
@@ -307,18 +231,19 @@ while True:
             pygame.quit()
 
     window.fill((20, 20, 20))
-    window.blit(game_screen, ((SCREEN_WIDTH - GAME_SCREEN_WIDTH) // 2, 0))
+    window.blit(game_screen, ((constants.SCREEN_WIDTH - constants.GAME_SCREEN_WIDTH) // 2, 0))
 
     font_game_over = pygame.font.SysFont("Comic Sans MS", 50)
     font_player = pygame.font.SysFont("Comic Sans MS", 30)
 
     text_game_over = font_game_over.render(f"Leaderboard", True, (255, 0, 0))
-    text_rect = text_game_over.get_rect(center=(GAME_SCREEN_WIDTH // 2, GAME_SCREEN_HEIGHT // 2))
+    text_rect = text_game_over.get_rect(center=(constants.GAME_SCREEN_WIDTH // 2, constants.GAME_SCREEN_HEIGHT // 2))
     window.blit(text_game_over, text_rect)
 
     for i in range(3):
         text_player = font_player.render(f"{places[i]}. {lines[i][0]}: {lines[i][1]}", True, (255, 255, 255))
-        text_rect = text_game_over.get_rect(center=(GAME_SCREEN_WIDTH // 2, GAME_SCREEN_HEIGHT // 2 + 75 + 30*i))
+        text_rect = text_game_over.get_rect(center=(
+            constants.GAME_SCREEN_WIDTH // 2, constants.GAME_SCREEN_HEIGHT // 2 + 75 + 30 * i))
         window.blit(text_player, text_rect)
 
     index = 0
@@ -330,7 +255,7 @@ while True:
 
     if index >= 3:
         text_player = font_player.render(f"{places[index]}. {lines[index][0]}: {lines[index][1]}", True, (255, 255, 255))
-        text_rect = text_game_over.get_rect(center=(GAME_SCREEN_WIDTH // 2, GAME_SCREEN_HEIGHT // 2 + 210))
+        text_rect = text_game_over.get_rect(center=(constants.GAME_SCREEN_WIDTH // 2, constants.GAME_SCREEN_HEIGHT // 2 + 210))
         window.blit(text_player, text_rect)
 
     clock.tick()
---
+++ util/Tetromino.py
@@ -0,0 +1,52 @@
+import random
+import pygame
+# import constants
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT = 300, 600
+GRID_WIDTH, GRID_HEIGHT = 10, 20
+BLOCK_SIZE = 30
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+class Tetromino:
+    COLORS = [
+        (0, 255, 255),  # I
+        (128, 0, 128),  # T
+        (255, 165, 0),  # L
+        (0, 0, 255),  # J
+        (255, 255, 0),  # O
+    ]
+
+    tetrominoes = [
+        [[1, 1, 1, 1]],  # I
+        [[1, 1, 1], [0, 1, 0]],  # T
+        [[1, 1, 1], [1, 0, 0]],  # L
+        [[1, 1, 1], [0, 0, 1]],  # J
+        [[1, 1], [1, 1]],  # O
+        [[0, 1, 1], [1, 1, 0]],
+        # [[0, 1, 0],[1, 1, 1],[0, 1, 0]]
+    ]
+
+    def __init__(self, forbidden_shape=None):
+        self.shape = random.choice(list(filter(lambda elem: elem != forbidden_shape, Tetromino.tetrominoes)))
+        self.color = random.choice(Tetromino.COLORS)
+        self.x = GRID_WIDTH // 2 - len(self.shape[0]) // 2
+        self.y = 0
+        self.rotation = 0
+
+    def rotate(self):
+        self.shape = [list(row) for row in zip(*self.shape[::-1])]
+
+    @staticmethod
+    def draw_tetromino(surface, tetromino):
+        for y, row in enumerate(tetromino.shape):
+            for x, cell in enumerate(row):
+                if cell != 0:
+                    pygame.draw.rect(surface, tetromino.color,
+                                     (
+                                         (tetromino.x + x) * BLOCK_SIZE,
+                                         (tetromino.y + y) * BLOCK_SIZE,
+                                         BLOCK_SIZE,
+                                         BLOCK_SIZE))
